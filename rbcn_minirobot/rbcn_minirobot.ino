cd05a08632c96cc0d37d4b465987b552ede8043f
Fragmented_nt <fragmentednt@zoho.com>
2018-10-17 20:02:47 +0900

    文字コード変更

--- rbcn_minirobot/rbcn_minirobot.ino
+++ rbcn_minirobot/rbcn_minirobot.ino
@@ -1,4 +1,4 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
+﻿// Visual Micro is in vMicro>General>Tutorial Mode
 // 
 /*
     Name:       rbcn_minirobot.ino
@@ -19,7 +19,7 @@
 // Define Functions below here or use other .ino or cpp files
 //
 
-MotorDriverTA8428KClass MD0, MD1, MD2;
+MotorDriverTA8428KClass MD0, MD1, MD2, MD3;
 
 
 // The setup() function runs once each time the micro-controller starts
@@ -34,12 +34,75 @@ void setup()
 	MD0.init(5, 6, true);
 	MD1.init(9, 10, true, true);
 	MD2.init(3, 11, true);
+	MD3.init(20, 21, true, false, false);
 
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
+	blinkLED();
+
+	TM0.Delay(9);
+	//printTimeSpan();
+	demo();
+}
 
 
+void printTimeSpan()
+{
+	static unsigned long prev_time = 0, current_time = 0;
+	current_time = TM0.Micros();
+	Serial.println(current_time - prev_time);
+	prev_time = TM0.Micros();
+}
+
+void blinkLED()
+{
+	static int ms = 0;
+	int mi = TM0.Millis();
+	if (mi - ms > 500)
+	{
+		ms = mi;
+		PINB |= _BV(5);	// Arduino nanoのPB5(D13番)に接続されているLEDを操作
+						// AVRの仕様でPINxに書き込むと出力が反転する(通常の出力レジスタはPORTx)
+	}
+}
+
+void demo()
+{
+	static int ms = 0;
+	int mi = TM0.Millis();
+	if (mi - ms < 2000)
+	{
+		MD0.set(0);
+		MD1.set(0);
+		MD2.set(0);
+		MD3.set(0);
+	}
+	else if (mi - ms < 4000)
+	{
+		MD0.set(100);
+		MD1.set(100);
+		MD2.set(100);
+		MD3.set(100);
+	}
+	else if (mi - ms < 6000)
+	{
+		MD0.set(0);
+		MD1.set(0);
+		MD2.set(0);
+		MD3.set(0);
+	}
+	else if (mi - ms < 8000)
+	{
+		MD0.set(-100);
+		MD1.set(-100);
+		MD2.set(-100);
+		MD3.set(-100);
+	}
+	else
+	{
+		ms = mi;
+	}
 }
